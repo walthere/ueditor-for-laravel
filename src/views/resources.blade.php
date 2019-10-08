ae4a8c9341a1010ebe920d6fc52a55b99b05d0fd
walthere <walthere@vip.qq.com>
2019-10-08 14:13:52 +0800

    fix auth

--- src/views/resources.blade.php
+++ src/views/resources.blade.php
@@ -1,4 +1,11 @@
-<script type="text/javascript" charset="utf-8" src="{!!asset('assets/js/UEditor/ueditor.config.js')!!}"></script>
+@if( null != $config  && '' != $config)
+    <script type="text/javascript" charset="utf-8"
+            src="{!!asset('assets/js/UEditor/'.$config.'.config.js')!!}"></script>
+@else
+    <script type="text/javascript" charset="utf-8"
+            src="{!!asset('assets/js/UEditor/user.config.js')!!}"></script>
+@endif
+
 <script type="text/javascript" charset="utf-8" src="{!!asset('assets/js/UEditor/ueditor.all.min.js')!!}"></script>
 <script type="text/javascript" charset="utf-8" src="{!!asset('assets/js/UEditor/lang/zh-cn/zh-cn.js')!!}"></script>
 
